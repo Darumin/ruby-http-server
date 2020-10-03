af2f7c7d2aa71450188ad6e77a79405c7ca6049b
Darumin <jandeo92@gmail.com>
2020-10-03 00:27:02 -0700

    Initial commit 🚀

---
+++ main.rb
@@ -0,0 +1,14 @@
+print "Hello world"
+
+# require 'socket'
+
+# hostname = 'localhost'
+# port = 1234
+
+# _sock = TCPSocket.open(hostname, port)
+
+# while line = _sock.gets
+#     puts line.chop
+# end
+
+# s.close
