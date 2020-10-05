1d97a1d8cb384c95e6e1e590674e432201a35a72
Darumin <jandeo92@gmail.com>
2020-10-05 01:18:23 -0700

    implemented a protocol that uses a dictionary

--- main.rb
+++ main.rb
@@ -6,43 +6,72 @@ class Server
         @socket = Socket.new(:INET, :STREAM, 0)
         @socket.bind(Addrinfo.tcp("127.0.0.1", 6789))
 
-        puts "Server successfully started."
+        @definitions = {
+            "someword" => "thedescription"
+        }
+
+        puts "✔️ Server successfully started."
         p @socket.local_address
     end
 
     def listen
         # listen for connections
         @socket.listen(1)
-        puts "Now listening...\n"
+        puts "Listening...\n"
 
         # part out socket.accept
         conn, conn_addr = @socket.accept
-        puts "Connection made to...\n"
+        puts "✔️ Client connected successfully.\n"
         p conn_addr
 
         # send and receive messages
         dictionary_protocol(conn)
-
-        puts "Now closing the socket."
-        @socket.close
     end
 
     def dictionary_protocol(connection)
-        loop do
-            data = connection.recvfrom(25)[0].chomp
-            if data == '' then 
-                break 
-            end
+        connection.send "You connected to the dictionary protocol.\r\n", 0
 
-            # from client, then back to server.
-            connection.send "you sent me this --> '#{data}'\r\n", 0
-            puts "max_bytes(25) --> '#{data}'"
+        loop do
+            data = connection.recvfrom(25)[0].chomp.split(/ /, 3)
+            command, key, setdesc = data[0], data[1], data[2]
+
             sleep 1
+
+            case
+            when command.eql?("SET")
+                if @definitions.has_key?(key)
+                    connection.send "Key already set.\r\n", 0
+                else
+                    @definitions[key] = setdesc
+                    connection.send "Key is set.\r\n", 0
+                end     
+            when command.eql?("GET")
+                if key == nil
+                    connection.send "No such word exists.\r\n", 0
+                else
+                    desc = @definitions[key]
+                    connection.send "#{desc}\r\n", 0
+                end
+            when command.eql?("CLEAR") 
+                @definitions.clear
+            when command.eql?("END") 
+                connection.send "Goodbye!\r\n", 0 
+                break 
+            else
+                connection.send "Invalid command.\r\n", 0
+            end
         end
+
+    end
+
+    def close
+        # when finished, call this
+        puts "Now closing the socket."
+        @socket.close
     end
         
 end
 
 serve = Server.new()
-serve.listen()
-# when finished, close the socket
+serve.listen
+serve.close
