19337e95e069a327c0043ef1b5fc937001c5dfa5
Darumin <jandeo92@gmail.com>
2020-10-04 23:39:38 -0700

    neater design

--- main.rb
+++ main.rb
@@ -1,32 +1,48 @@
 require 'socket'
 
+class Server
+    def initialize
+        # bind socket and start server
+        @socket = Socket.new(:INET, :STREAM, 0)
+        @socket.bind(Addrinfo.tcp("127.0.0.1", 6789))
 
-# start socket
-socket = Socket.new(:INET, :STREAM, 0)
-socket.bind(Addrinfo.tcp("127.0.0.1", 6789))
-puts "Socket service started."
-p socket.local_address
+        puts "Server successfully started."
+        p @socket.local_address
+    end
 
-# listen for connections
-socket.listen(1)
-puts "Now listening...\n"
+    def listen
+        # listen for connections
+        @socket.listen(1)
+        puts "Now listening...\n"
 
-# part out socket.accept
-conn, conn_addr = socket.accept
-puts "Connected to PuTTY.\n"
-p conn_addr
+        # part out socket.accept
+        conn, conn_addr = @socket.accept
+        puts "Connection made to...\n"
+        p conn_addr
 
-# send and receive messages
-loop do
-    data = conn.recvfrom(25)[0].chomp
-    if data == '' then break end
-    
-    # to client, and back to server.
-    conn.send "you sent me this --> '#{data}'\n", 0
-    puts "max_bytes(25) --> '#{data}'"
-    sleep 1
+        # send and receive messages
+        dictionary_protocol(conn)
+
+        puts "Now closing the socket."
+        @socket.close
+    end
+
+    def dictionary_protocol(connection)
+        loop do
+            data = connection.recvfrom(25)[0].chomp
+            if data == '' then 
+                break 
+            end
+
+            # from client, then back to server.
+            connection.send "you sent me this --> '#{data}'\r\n", 0
+            puts "max_bytes(25) --> '#{data}'"
+            sleep 1
+        end
+    end
+        
 end
 
+serve = Server.new()
+serve.listen()
 # when finished, close the socket
-puts "Now closing the socket."
-socket.close
