99883a9186198e7a108507fa7e4d0158018fd481
Darumin <jandeo92@gmail.com>
2020-10-07 18:59:45 -0700

    further abstractions with some funky assertions

--- rubby.rb
+++ rubby.rb
@@ -1,75 +1,74 @@
 require "socket"
+require "singleton"
 
-class Server
+class RubyServer
+    include Singleton
+    attr_reader :dictionary
+    attr_writer :dictionary
+
     def initialize
-        # bind socket and start server
         @socket = Socket.new(:INET, :STREAM, 0)
         @socket.bind(Addrinfo.tcp("127.0.0.1", 6789))
 
-        @definitions = {
-            "someword" => "thedescription"
-        }
-
-        puts "✔️ Server successfully started."
+        @dictionary = dictionary
         p @socket.local_address
     end
 
     def listen
-        # listen for connections
         @socket.listen(1)
-        puts "Listening...\n"
-        # part out socket.accept
-        conn, conn_addr = @socket.accept
+        puts "✔️ Server successfully started."
+        puts "Listening for incoming connections...\n"
+        
+        # TODO: Implement a backlog.
+        @client, client_addr = @socket.accept
         puts "✔️ Client connected successfully.\n"
-        p conn_addr
 
-        # send and receive messages
-        dictionary_protocol(conn)
+        p client_addr 
     end
 
-    def dictionary_protocol(connection)
-        connection.send "You connected to the dictionary protocol.\r\n", 0
+    def dictionary_protocol
+        if @dictionary == nil
+            return
+        end
 
         loop do
-            data = connection.recvfrom(128)[0].chomp.split(/ /, 3)
+            data = @client.recvfrom(128)[0].chomp.split(/ /, 3)
             command, key, setdesc = *data
-            puts command, key, setdesc
 
             sleep 1
 
             case
             when command.eql?("SET")
-                @definitions[key] = setdesc
-                connection.send "Key `#{key}` has been set to `#{setdesc}`.\r\n", 0   
+                @dictionary[key] = setdesc
+                @client.send "Key `#{key}` has been set to `#{setdesc}`.\r\n", 0   
             when command.eql?("GET")
                 if key == nil
-                    connection.send "No such word exists.\r\n", 0
+                    @client.send "No such word exists.\r\n", 0
                 else
-                    desc = @definitions[key]
-                    connection.send "ANSWER #{desc}\r\n", 0
+                    desc = @dictionary[key]
+                    @client.send "ANSWER #{desc}\r\n", 0
                 end
             when command.eql?("CLEAR") 
-                @definitions.clear
-            when command.eql?("END") 
-                connection.send "Goodbye!\r\n", 0 
+                @dictionary.clear
+            when command.eql?("END")
                 break 
             else
-                connection.send "Invalid command.\r\n", 0
+                @client.send "Invalid command\r\n", 0
             end
         end
 
     end
 
     def close
-        # when finished, call this
-        puts "Now closing the socket."
+        puts "Successfully closed server."
         @socket.close
     end
 end
 
-
-serve = Server.new()
-serve.listen
-serve.close
+rubby = RubyServer.instance
+rubby.dictionary = {"someword" => "somedefinition", "anotherword" => "anotherdefinition"}
+rubby.listen
+rubby.dictionary_protocol
+rubby.close
 
 
--- rubby_test.rb
+++ rubby_test.rb
@@ -4,11 +4,29 @@ require "socket"
 include Socket::Constants
 
 class TestRubby < Test::Unit::TestCase
-    def test_this_test
-        client = Socket.new(AF_INET, SOCK_STREAM, 0)
-        addr = Socket.pack_sockaddr_in(6789, "127.0.0.1")
-        client.connect(addr)
-        client.send "GET someword", 0
-        assert(line == "thedescription")
+    def setup
+        @client = Socket.new(AF_INET, SOCK_STREAM, 0)
+        @addr = Socket.pack_sockaddr_in(6789, "127.0.0.1")
+        @client.connect(@addr)
+    end
+
+    def test_answer
+        @client.send "GET someword", 0
+        line = @client.recvfrom(128)[0].chomp
+        assert_equal(line, "ANSWER somedefinition")
+        @client.send "RUBBY", 0
+        line = @client.recvfrom(128)[0].chomp
+        assert_equal(line, "Invalid command")
+    end
+
+    # def test_invalid_command
+    #     @client.send "RUBBY", 0
+    #     line = @client.recvfrom(128)[0].chomp
+    #     assert_equal(line, "Invalid command")
+    # end
+
+    def teardown
+        @client.send "END", 0
+        @client.close
     end
 end
